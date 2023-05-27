5ee59004ecacee050232ccd5a6be4b06b5cdd850
Oluchi <okparabon@gmail.com>
2023-05-27 11:55:12 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -46,7 +46,7 @@ class App
     puts 'Enter Student Classroom'
     classroom = gets.chomp
     student = Student.new(classroom, age, name, parent_permission: parent_permission)
-    @people.push(student)
+    @peoples.push(student)
   end
 
   def create_teacher

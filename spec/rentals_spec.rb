e9b2f7bf787cdaa77f46033b91fbb18c65259f1d
Luchy-B <okparabon@gmail.com>
2023-06-02 10:53:36 +0100

    Add rental_spec file

--- spec/rentals_spec.rb
+++ spec/rentals_spec.rb
@@ -1,15 +1,1 @@
-describe '#initialize' do
-  it 'sets the date of the rental' do
-    expect(rental.date).to eq(date)
-  end
-
-  it 'associates the rental with the person' do
-    expect(rental.person).to eq(person)
-    expect(person.rentals).to include(rental)
-  end
-
-  it 'associates the rental with the book' do
-    expect(rental.book).to eq(book)
-    expect(book.rentals).to include(rental)
-  end
-end
+#rentals_spec.rb

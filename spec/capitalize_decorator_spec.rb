07c7e31bce1a593ebfb6a298b61db896331b3c83
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-06-02 18:24:15 +0530

    ADD:capitalize & teacher test file

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,1 +1,13 @@
 # capitaliza_decorator_specs.rb
+require './decorator'
+describe CapitalizeDecorator do
+  name = CapitalizeDecorator.new('meghan_trainor')
+  context 'given an instance' do
+    it 'returns correct title' do
+      expect(name.nameable).to eq('meghan_trainor')
+    end
+    it 'capitalize the input' do
+      expect(name.nameable.capitalize).to eq('Meghan_trainor')
+    end
+  end
+end
